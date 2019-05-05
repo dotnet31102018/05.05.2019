f2050946b614d4287b0efae6a9d67ffd787696ca
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-05-05 18:12:51 +0300

    Add files via upload

---
+++ JSON_tostring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace EF0505
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person()
+            {
+                Id = 1,
+                Name = "Roy",
+                Age = 12,
+                City_ID = 12
+            };
+            Console.WriteLine(JsonConvert.SerializeObject(p));
+        }
+    }
+}
